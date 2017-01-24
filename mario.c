e70793c50b697eac367b0e499e9250a0e94c93ee
Rootick <gotigsable@gmail.com>
2017-01-24 21:46:33 +0100

    dzialaaa

--- mario.c
+++ mario.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void space();
+void space();   //te dwie funkcje drukują tylko znaki " " i "#"
 void hash();
 
 int main(void)
 {
     printf("Height: ");
+    int h;
     while(true)
     {
-        int h = get_int();
+        h = get_int();
         if(h >= 0 && h < 24)
         {
             break;
@@ -19,7 +20,20 @@ int main(void)
             printf("Height: ");
         }
     }
+    
+    int h2 = 2;    // tu wcześniej było h2 = h XD   jest h2 = 2, bo ilość hashów na szczycie jest stała, później tylko rośnie odwrotnie od h
+    
+    for(int i = h - 1; i >= 0; i--)   //tutaj h - 1 bo zauważyłem, że tyle razy i musi spaść, żeby był blok samych hashów i wysokość była równa
+    {
+        space(i);
+        hash(h2);
+        h2 = h2 + 1;     //dla równego prawego boku
+        //hash(h2 - 2);   wcześniej h2 to było h, tylko wykorzystane w innym kierunku
+        //h2 = h2 + 1;    to był dziwny pomysł dx
+        printf("\n");
+    }
 }
+
 void space(n)
 {
     for(int i = 0; i < n; i++)
