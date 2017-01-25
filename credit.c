9ad0d1eb59d1292745804dea862916bbda7bff41
Rootick <gotigsable@gmail.com>
2017-01-25 17:44:38 +0100

    czytelność poprawiona

--- credit.c
+++ credit.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
-#include <cs50.h>
-#include <math.h>
+#include <stdio.h>//jak narazie, program sprawdza ilość cyfr, i powinien sprawdzać też
+#include <cs50.h> //częściowo zgodność z kartami AMEX, ale gubi typ long long przy
+#include <math.h> //przeniesieniu do funkcji ;-;
 
 long long check_comp();
 
 int main(void)
 {
     printf("Number: ");
-    long long num = get_long_long();
-    long long num2 = num;
-    printf("\n%lli\n", num2);
-    long long p = pow(10, 13);
-    printf("%lli\n", p);
-    long long countd = 0; //countd=digit count
-    while(num != 0) //pętla liczy cyfry
+    long long num_loop = get_long_long();
+    long long num_card = num_loop;//kopia, num_loop zmienia wartość po pętli,
+    //num_card to główna zmienna z numerem karty
+    
+    long long countd = 0;
+    while(num_loop != 0)
     {
-        num /= 10;
+        num_loop /= 10;
         countd++;
     }
-    printf("number of digits %lli\n", countd); //sprawdzenie,czy dobrze policzyło
-    long long a = floor(num2 / pow(10, 13)); //to samo dzialanie co w funkcji, dziala
-    long long b = a - 37;
-    printf("%lli, %lli\n", a, b);
-    printf("\n%lli\n", num2);
-    check_comp(num2, countd);  //cała reszta nie działa, nie kompiluje
-    return 0;                 //ta funkcja ma sprawdzać, do jakiej firmy należy karta
-}                             //po ilości liczb i początkowych dwóch cyfrach
+    printf("number of digits %lli\n", countd);
+    
+    long long test = floor(num_card / pow(10, 13) - 37);//test działania, z kartą o
+    printf("Wynik testu: %lli\n", test);                //numerze 378282246310005 powinien dać 0,
+    //i daje 0
+    check_comp(num_card, countd);
+    //w przeciwieństwie do funkcji ;-;
+    return 0;
+}
 
 
 
-long long check_comp(n, c)
-{
+long long check_comp(n, c)//num_card, countd
+{   //to niżej to pełne sprawdzenie, bo karty AMEX mają 15 cyfr, a dwie pierwsze to 34 lub 37
     //if(((c == 15) && (floor(n / pow(10, 13))) == 34) || (floor(n / pow(10, 13)) == 37))
-    printf("zmienne: %i, %i ", n, c);
-    long long lol = floor(n / pow(10, 13));
-    if((c == 15) && (floor(n / pow(10, 13))) - 37 == 0)
+    printf("zmienne: %i, %i \n", n, c);//tu widać, że n i c, czyli num_card i countd
+//mają typ integer, chociaż wcześniej były long long, przez co n num_card się rozwala
+    if((c == 15) && (floor(n / pow(10, 13))) == 37)
     {
-        printf("lol dziala\n");
+        printf("karta AMEX rozpoznana\n");
     }
     else
     {
-        printf("jednak nie, %lli\n", lol);
-    }
+        printf("karta niewlasciwa, dwie pierwsze cyfry: %f\n", floor(n / pow(10, 13)));
+    }//przy else zwraca -1, niezależnie od typu finalnego, sprawdzałem
     return 0;
 }
