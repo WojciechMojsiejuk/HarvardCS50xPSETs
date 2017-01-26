ce6f3b2bbb5ff0fa4f89304bdc0b3e9d11855ea3
Rootick <gotigsable@gmail.com>
2017-01-26 16:00:46 +0100

    square of every 2nd digit implemented

--- credit.c
+++ credit.c
@@ -29,7 +29,6 @@ int main(void)
 
 long long check_comp(long long n,long long c)//num_card, countd
 {   
-    printf("zmienne: %lli, %lli \n", n, c);
     if(((c == 15) && (floor(n / pow(10, 13))) == 34) || (floor(n / pow(10, 13)) == 37))
     {
         printf("AMEX\n");
@@ -54,13 +53,21 @@ long long check_algo(long long n)//n = num_card
 {
     long long sum = 0, rem, num_algo;//sum = suma co drugiej cyfry, rem = reszta (czyli kolejna cyfra, num_algo = kopia num na wszelki wypadek)
     num_algo = n;
-    num_algo /= 10;
+    num_algo /= 10;//pominięcie ostatniej cyfry
     while(num_algo != 0)
     {
-        rem = num_algo % 10;
-        sum = sum + rem;
-        num_algo = num_algo / 100;
+        rem = num_algo % 10;//rem, czyli każda druga cyfra od końca
+        if(rem*2 / 10 >= 1)//sprawdzenie, czy dwukrotność jest dwucyfrowa
+        {
+            sum = sum + floor(rem*2 / 10);//dodanie pierwszej cyfry
+            sum = sum + rem*2 % 10;//dodanie drugiej cyfry
+        }
+        else
+        {
+            sum = sum + rem*2;//jeśli dwukrotność jednocyfrowa, zwykłe dodanie
+        }
+        num_algo = num_algo / 100;//przejście do kolejnej drugiej cyfry
     }
-    printf("suma co drugich cyfr: %lli\n", sum);
+    printf("suma co drugich cyfr pomnożonych przez 2: %lli\n", sum);
     return 0;
 }
