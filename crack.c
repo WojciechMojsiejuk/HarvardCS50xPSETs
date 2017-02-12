e515f9a2ba12ff9bafd6f1cedf17cfe649fe5f61
Rootick <gotigsable@gmail.com>
2017-02-12 19:20:52 +0100

    single character cracking

--- crack.c
+++ crack.c
@@ -15,22 +15,39 @@ int main(int argc, string argv[])
     string hash = argv[1];
     
     //thash-string testowy do porownywania z hashem
-    string thash;
-    
+    string thash, thash2;
+   
     //rofl = 50fkUxYHbnXGw   
     //a-50Dtm00K6zwZg   ???
+    //A-50OzIExR0.TSw
     char salt[3] = {hash[0], hash[1], '\0'};
     for(char i = 'a'; i <= 'c'; i++)
     {
         thash = crypt(&i, salt);
         printf("%s\n",thash);
         printf("%s\n",hash);
+        printf("%c\n",i);
+
         if(strcmp(hash, thash) == 0)
         {
             printf("dziala\n");
             return 0;
         }
-        
+
     }  
+    for(char j = 'A'; j <= 'C'; j++)
+    {
+        thash2 = crypt(&j,salt);
+        printf("%s\n",thash2);
+        printf("%s\n",hash);
+        printf("%c\n",j);
+
+        if(strcmp(hash, thash2) == 0)
+        {
+            printf("DZIALA\n");
+            return 0;
+        }
+    }  
+
 
 }
